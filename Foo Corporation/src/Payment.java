5791e7334bd57666572b82333184d4c4f7ba631b
Alberto <Alberto@10.33.193.4>
2015-09-20 14:14:27 -0500

    PDF 2

---
+++ Foo Corporation/src/Payment.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Payment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este programa te ayudara a calcular la paga del empleado");
+		
+		double basePay = 0.0;
+		double hours = 0.0;
+		double payment = 0.0;
+		double maxHours = 40.0;
+		
+		Scanner user_input = new Scanner (System.in);
+		System.out.println("Ingresa el salario base");
+			basePay = user_input.nextDouble();
+		System.out.println("Ingresa las horas trabajadas");
+			hours = user_input.nextDouble();
+		
+		if (basePay<8.0) { 
+			hours = 61;
+			System.out.println("error");
+		}
+		else {
+			
+		}
+			
+		if (hours<=40.0) {
+			payment = hours*basePay;
+			System.out.println("La paga del trabajador por las" + hours + "horas trabajadas es de $" + payment + "dolares" );
+		}
+		else {
+			if (hours<60.0) {
+				
+				payment = (maxHours*basePay) + ((hours-40)*(basePay*1.5));
+				System.out.println("La paga del trabajador por las" + hours + "horas trabajadas es de $" + payment + "dolares" );
+			}
+			else {
+				System.out.println("error");
+			}
+		}
+			
+			
+		
+		
+	}
+
+}
